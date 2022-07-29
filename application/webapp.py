960c805246dc27705319b98ec702e3873d4b3362
pankwings <pankwings@gmail.com>
2022-07-29 21:17:35 +0000

    Removed the wifi and bluetooth data

--- application/webapp.py
+++ application/webapp.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from providers.temperature import TemperatureSensor
 #from providers.motion import Motion
-from providers.bluetooth import Bluetooth
+#from providers.bluetooth import Bluetooth
 from providers.wifi import Wifi
 import RPi.GPIO as GPIO
 import time
@@ -15,18 +15,18 @@ temperature = TemperatureSensor(4)
 #motion = Motion(17)
 #motion.run()
 
-bluetooth = Bluetooth()
-bluetooth.run()
+#bluetooth = Bluetooth()
+#bluetooth.run()
 
-wifi = Wifi(os.environ['NETWORK_TO_SCAN'])
-wifi.run()
+#wifi = Wifi(os.environ['NETWORK_TO_SCAN'])
+#wifi.run()
 
 app = Flask(__name__)
 
 @app.route("/metrics")
 def metrics():
     #metrics = temperature.read() + motion.read() + bluetooth.read() + wifi.read()
-    metrics = temperature.read() + bluetooth.read() + wifi.read()
+    metrics = temperature.read() # 
     result = [metric.to_prometheus() for metric in metrics]
     return "\n".join(result), 200, {'Content-Type': 'text/plain; charset=utf-8'}
 
